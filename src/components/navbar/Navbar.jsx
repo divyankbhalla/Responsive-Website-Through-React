5930aa6673fc5c32f06c908942e7da6cf7e35f44
Divyank Bhalla <81313107+divyankbhalla@users.noreply.github.com>
2021-11-17 13:50:13 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './Navbar.css';
+import logo from '../../assets/logo.svg';
 
 function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className='gpt3__navbar'>
+            <div className='gpt3__navbar-links'>
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt='logo' />
+                </div>
+            </div>
         </div>
     )
 }
