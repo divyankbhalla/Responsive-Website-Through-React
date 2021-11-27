4b65c02b1650a766b4a3527d4a64d61d913e6136
Divyank Bhalla <81313107+divyankbhalla@users.noreply.github.com>
2021-11-27 15:02:37 +0530

    Update Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,10 @@ import './Header.css';
 
 function Header() {
     return (
-        <div>
-          Header  
+        <div className="gpt3__header section__padding" id="home">
+          <div className="gpt3__header-content">
+            <h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
+          </div>
         </div>
     )
 }
